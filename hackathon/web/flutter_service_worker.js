ea8bf53848a1e06ce788ecfbc8407babcd6d2a60
ashank96 <bharatiashank1996@gmail.com>
2021-03-20 12:22:09 +0530

    some more fixes

--- hackathon/web/flutter_service_worker.js
+++ hackathon/web/flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "native_bridge.js": "fec7fea879a9060a22b1d80dca752874",
 "index.html": "218fa13a1d28ed14c8bea4f09ee79574",
 "/": "218fa13a1d28ed14c8bea4f09ee79574",
-"main.dart.js": "f2e2b49bbea3706299ea62db20a9ff82",
+"main.dart.js": "99cc92a2765cd6fdea5b2bbe06080a12",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "b2da32757322e11f0e061b28321b83b1",
